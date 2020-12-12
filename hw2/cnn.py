9ad97f29c70de5048e3c71c5029afbe191e926de
Adina Katz <48714000+adinak@users.noreply.github.com>
2020-12-12 19:55:03 +0200

    all done part for experiments

--- hw2/cnn.py
+++ hw2/cnn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import itertools as it
+
 # from .blocks import ReLU, LeakyReLU, Linear
 
 ACTIVATIONS = {"relu": nn.ReLU, "lrelu": nn.LeakyReLU}
@@ -16,17 +17,17 @@ class ConvClassifier(nn.Module):
     """
 
     def __init__(
-        self,
-        in_size,
-        out_classes: int,
-        channels: list,
-        pool_every: int,
-        hidden_dims: list,
-        conv_params: dict = {},
-        activation_type: str = "relu",
-        activation_params: dict = {},
-        pooling_type: str = "max",
-        pooling_params: dict = {},
+            self,
+            in_size,
+            out_classes: int,
+            channels: list,
+            pool_every: int,
+            hidden_dims: list,
+            conv_params: dict = {},
+            activation_type: str = "relu",
+            activation_params: dict = {},
+            pooling_type: str = "max",
+            pooling_params: dict = {},
     ):
         """
         :param in_size: Size of input images, e.g. (C,H,W).
@@ -81,38 +82,42 @@ class ConvClassifier(nn.Module):
         stride = self.conv_params['stride']
         conv_kernel = self.conv_params['kernel_size']
         pool_kernel = self.pooling_params['kernel_size']
-        
+
         N = len(self.channels)
         P = self.pool_every
-        
+
         for i in range(N):
             # conv layer
-            layers.append(nn.Conv2d(in_channels=in_channels, out_channels=self.channels[i], **self.conv_params))
+            layers.append(nn.Conv2d(in_channels=in_channels,
+                                    out_channels=self.channels[i],
+                                    **self.conv_params))
 
             # relu / leaky relu layer
             if self.activation_type == "lrelu":
                 layers.append(nn.LeakyReLU(**self.activation_params))
-            else: 
+            else:
                 layers.append(nn.ReLU(**self.activation_params))
 
             # update dimensions
             temp_dim = (2 * padding - conv_kernel)
-            in_h = int(torch.floor(torch.tensor((in_h + temp_dim) / stride)) + 1) 
-            in_w = int(torch.floor(torch.tensor((in_w + temp_dim) / stride)) + 1)
+            in_h = int(
+                torch.floor(torch.tensor((in_h + temp_dim) / stride)) + 1)
+            in_w = int(
+                torch.floor(torch.tensor((in_w + temp_dim) / stride)) + 1)
             in_channels = self.channels[i]
-            
+
             # pooling layer
-            if (i + 1) % P == 0:    
+            if (i + 1) % P == 0:
                 if self.pooling_type == "avg":
                     layers.append(nn.AvgPool2d(**self.pooling_params))
                 else:
                     layers.append(nn.MaxPool2d(**self.pooling_params))
-            
-                #update dimensions
-                in_h = int(torch.floor(torch.tensor((in_h - pool_kernel) / pool_kernel)) + 1)
-                in_w = int(torch.floor(torch.tensor((in_w - pool_kernel) / pool_kernel)) + 1)
-        
-        
+
+                # update dimensions
+                in_h = int(torch.floor(
+                    torch.tensor((in_h - pool_kernel) / pool_kernel)) + 1)
+                in_w = int(torch.floor(
+                    torch.tensor((in_w - pool_kernel) / pool_kernel)) + 1)
         # ========================
         self.in_size = self.channels[-1], in_h, in_w
         seq = nn.Sequential(*layers)
@@ -126,23 +131,25 @@ class ConvClassifier(nn.Module):
         #  the first linear layer.
         #  The last Linear layer should have an output dim of out_classes.
         # ====== YOUR CODE: ======
-        first_in_dims = int(self.channels[-1] * self.in_size[1] * self.in_size[2])
-        
-        for indims, outdims in zip([first_in_dims] + self.hidden_dims, self.hidden_dims):
+        first_in_dims = int(
+            self.channels[-1] * self.in_size[1] * self.in_size[2])
+
+        for indims, outdims in zip([first_in_dims] + self.hidden_dims,
+                                   self.hidden_dims):
             layers.append(nn.Linear(indims, outdims))
-            
+
             if self.activation_type == "lrelu":
                 layers.append(nn.LeakyReLU(**self.activation_params))
-            else: 
-                layers.append(nn.ReLU(**self.activation_params)) 
-                
+            else:
+                layers.append(nn.ReLU(**self.activation_params))
+
         layers.append(nn.Linear(self.hidden_dims[-1], self.out_classes))
         # ========================
         seq = nn.Sequential(*layers)
         return seq
 
     def forward(self, x):
-        # TODO: Implement the forward pass.
+        # DONE: Implement the forward pass.
         #  Extract features from the input, run the classifier on them and
         #  return class scores.
         # ====== YOUR CODE: ======
@@ -159,15 +166,15 @@ class ResidualBlock(nn.Module):
     """
 
     def __init__(
-        self,
-        in_channels: int,
-        channels: list,
-        kernel_sizes: list,
-        batchnorm=False,
-        dropout=0.0,
-        activation_type: str = "relu",
-        activation_params: dict = {},
-        **kwargs,
+            self,
+            in_channels: int,
+            channels: list,
+            kernel_sizes: list,
+            batchnorm=False,
+            dropout=0.0,
+            activation_type: str = "relu",
+            activation_params: dict = {},
+            **kwargs,
     ):
         """
         :param in_channels: Number of input channels to the first convolution.
@@ -194,7 +201,7 @@ class ResidualBlock(nn.Module):
 
         self.main_path, self.shortcut_path = None, None
 
-        # TODO: Implement a generic residual block.
+        # DONE: Implement a generic residual block.
         #  Use the given arguments to create two nn.Sequentials:
         #  - main_path, which should contain the convolution, dropout,
         #    batchnorm, relu sequences (in this order).
@@ -208,11 +215,60 @@ class ResidualBlock(nn.Module):
         #  - Don't create layers which you don't use! This will prevent
         #    correct comparison in the test.
         # ====== YOUR CODE: ======
-        
+
+        # We're assuming there are no dilation, stride or padding.
+        # The default values we'll be used to calculate the conv.
+
         # main path
-        
-        
-        
+        # conv -> bias=True
+        # dropout
+        # batchnorm
+        # relu
+        # conv
+
+        N = len(channels)
+        main_layers = []
+        last_in_channels = in_channels
+
+        for i in range(N - 1):
+            # conv layer
+            main_layers.append(nn.Conv2d(in_channels=last_in_channels,
+                                         out_channels=channels[i],
+                                         kernel_size=kernel_sizes[i],
+                                         padding=int((kernel_sizes[i] - 1) / 2),
+                                         bias=True))
+            # dropout
+            main_layers.append(nn.Dropout2d(dropout))
+            # batchnorm
+            if batchnorm:
+                main_layers.append(nn.BatchNorm2d(num_features=channels[i]))
+            # relu / leaky relu layer
+            if activation_type == "lrelu":
+                main_layers.append(nn.LeakyReLU(**activation_params))
+            else:
+                main_layers.append(nn.ReLU(**activation_params))
+
+            # update dimensions
+            last_in_channels = channels[i]
+
+        main_layers.append(nn.Conv2d(in_channels=last_in_channels,
+                                     out_channels=channels[-1],
+                                     kernel_size=kernel_sizes[-1],
+                                     padding=int((kernel_sizes[-1] - 1) / 2),
+                                     bias=True))
+        self.main_path = nn.Sequential(*main_layers)
+
+        # shortcut path
+        # skip connection
+        # 1X1 conv -> bias=False
+        if in_channels == channels[-1]:
+            self.shortcut_path = nn.Sequential(nn.Identity())
+        else:
+            self.shortcut_path = nn.Sequential(nn.Conv2d(in_channels=in_channels,
+                                                         out_channels=channels[-1],
+                                                         kernel_size=1,
+                                                         bias=False))
+
         # ========================
 
     def forward(self, x):
@@ -224,15 +280,15 @@ class ResidualBlock(nn.Module):
 
 class ResNetClassifier(ConvClassifier):
     def __init__(
-        self,
-        in_size,
-        out_classes,
-        channels,
-        pool_every,
-        hidden_dims,
-        batchnorm=False,
-        dropout=0.0,
-        **kwargs,
+            self,
+            in_size,
+            out_classes,
+            channels,
+            pool_every,
+            hidden_dims,
+            batchnorm=False,
+            dropout=0.0,
+            **kwargs,
     ):
         """
         See arguments of ConvClassifier & ResidualBlock.
@@ -247,7 +303,7 @@ class ResNetClassifier(ConvClassifier):
         in_channels, in_h, in_w, = tuple(self.in_size)
 
         layers = []
-        # TODO: Create the feature extractor part of the model:
+        # DONE: Create the feature extractor part of the model:
         #  [-> (CONV -> ACT)*P -> POOL]*(N/P)
         #   \------- SKIP ------/
         #  For the ResidualBlocks, use only dimension-preserving 3x3 convolutions.
@@ -258,15 +314,54 @@ class ResNetClassifier(ConvClassifier):
         #    without a POOL after them.
         #  - Use your own ResidualBlock implementation.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        N = len(self.channels)
+        P = self.pool_every
+        pool_kernel = self.pooling_params['kernel_size']
+
+        for i in range(N // P):
+
+            layers.append(ResidualBlock(in_channels=in_channels,
+                                        channels=self.channels[i * P:P * (i + 1)],
+                                        kernel_sizes=[3] * P,
+                                        batchnorm=self.batchnorm,
+                                        dropout=self.dropout,
+                                        activation_type=self.activation_type,
+                                        activation_params=self.activation_params))
+
+            # update dimensions
+            in_channels = self.channels[i]
+
+            # pooling layer
+            if self.pooling_type == "avg":
+                layers.append(nn.AvgPool2d(**self.pooling_params))
+            else:
+                layers.append(nn.MaxPool2d(**self.pooling_params))
+
+            in_h = int(torch.floor(
+                torch.tensor((in_h - pool_kernel) / pool_kernel)) + 1)
+            in_w = int(torch.floor(
+                torch.tensor((in_w - pool_kernel) / pool_kernel)) + 1)
+
+        if N % P != 0:
+            size = N // P
+            layers.append(ResidualBlock(in_channels=self.channels[size * P - 1],
+                                        channels=self.channels[size * P:],
+                                        kernel_sizes=[3] * (N % P),
+                                        batchnorm=self.batchnorm,
+                                        dropout=self.dropout,
+                                        activation_type=self.activation_type,
+                                        activation_params=self.activation_params))
+
         # ========================
+        self.in_size = self.channels[-1], in_h, in_w
         seq = nn.Sequential(*layers)
         return seq
 
 
 class YourCodeNet(ConvClassifier):
     def __init__(self, in_size, out_classes, channels, pool_every, hidden_dims):
-        super().__init__(in_size, out_classes, channels, pool_every, hidden_dims)
+        super().__init__(in_size, out_classes, channels, pool_every,
+                         hidden_dims)
 
     # TODO: Change whatever you want about the ConvClassifier to try to
     #  improve it's results on CIFAR-10.
